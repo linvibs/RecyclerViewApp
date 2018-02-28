a8adcff3bbfe39da2aa020b13fda6822fc336d86
linvibs <linvibs@gmail.com>
2018-03-01 00:43:11 +0100

    Added Layout Manager and Set the Adapter on RecyclerView ---------------------------------------------------------- 1 - MainActivity.java 1.1 - Added the LayoutManager, that will inform, how the views will be positioned by choosing the desired LayoutManager i.e. Vertically, horizontally etc... 1.2 - Created data source 'languages' of type "String Array". 1.3 - Set the Adapter on the RecyclerView i.e. 'programmingList.setAdapter', but first getting an new Instance of 'ProgrammingAdapter', by passing 'languages' as a param to it.

--- app/src/main/java/com/example/hejvibs/recyclerviewapp/MainActivity.java
+++ app/src/main/java/com/example/hejvibs/recyclerviewapp/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.hejvibs.recyclerviewapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 
 public class MainActivity extends AppCompatActivity {
@@ -11,5 +12,10 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         RecyclerView programmingList = findViewById(R.id.programmingList);
+        programmingList.setLayoutManager(new LinearLayoutManager(this));
+
+        // Created a data source i.e. String ArrayList
+        String[] languages = {"Java", "Kotlin", "Swift", "Python", "C#", "JavaScript", "JSON", "SQLite", "XML", "Java", "Kotlin", "Swift", "Python", "C#", "JavaScript", "JSON", "SQLite", "XML"};
+        programmingList.setAdapter(new ProgrammingAdapter(languages));
     }
 }
